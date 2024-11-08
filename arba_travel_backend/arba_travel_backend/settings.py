e35ad56c45480ed4c285984a4a0ce2eb04c3053f
muqri shamsuddin <muqri.shamsuddin@uemsunrise.com>
2024-11-08 11:42:48 +0800

    Setup frondend

--- arba_travel_backend/arba_travel_backend/settings.py
+++ arba_travel_backend/arba_travel_backend/settings.py
@@ -132,3 +132,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'api.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- arba_travel_frontend/src/components/PostList.js
+++ arba_travel_frontend/src/components/PostList.js
@@ -18,14 +18,30 @@ const PostList = () => {
   return (
     <div>
       <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.caption}</h2>
-            <img src={post.image} alt="Post" style={{ width: "100px" }} />
-          </li>
-        ))}
-      </ul>
+      <table border="1" style={{ width: "100%", textAlign: "left" }}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Caption</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.caption}</td>
+              <td>
+                <img
+                  src={`http://127.0.0.1:8000${post.image}`}
+                  alt="Post"
+                  style={{ width: "100px" }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
